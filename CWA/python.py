b23067a234e610b24eb83eb7f880665f0770bed3
BroaderImpact <BroaderImpact@users.noreply.github.com>
2020-08-14 18:31:52 -0500

    creation of python file for CWA technical

---
+++ CWA/python.py
@@ -0,0 +1,11 @@
+# Python
+# Instructions
+# ● You have been provided with a sample text file of Chicago firefighters. For this exercise,
+# please use that data set.
+# ● Please provide us with the code for the script that opens the included text file and
+# ● does the following:
+# ○ Converts the delimiter from comma (“,”) to tab.
+# ○ Converts all of the header fields to uppercase.
+# ○ Adds a column named “VERIFIED” to the header
+# ○ Populates all rows in the “VERIFIED” column with “Y”
+# ● You may use either Python 2 or 3 for your work.

d902736461ab148cf37c28004a40bc96dd0b2d7a
BroaderImpact <BroaderImpact@users.noreply.github.com>
2020-08-14 18:37:58 -0500

    reading in cfd.csv

--- CWA/python.py
+++ CWA/python.py
@@ -2,9 +2,14 @@
 # Instructions
 # ● You have been provided with a sample text file of Chicago firefighters. For this exercise,
 # please use that data set.
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sb
+cfd = pd.read_csv("C:\Users\Christine\Google Drive - c.amuzie\Applications\CWA\cfd.csv")
 # ● Please provide us with the code for the script that opens the included text file and
 # ● does the following:
 # ○ Converts the delimiter from comma (“,”) to tab.
+
 # ○ Converts all of the header fields to uppercase.
 # ○ Adds a column named “VERIFIED” to the header
 # ○ Populates all rows in the “VERIFIED” column with “Y”

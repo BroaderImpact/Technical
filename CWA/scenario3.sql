1a7e536b3188a87132e8b6f1e0f5c0c6c9e2129a
BroaderImpact <BroaderImpact@users.noreply.github.com>
2020-08-15 15:55:44 -0500

    make it make sense

--- CWA/scenario3.sql
+++ CWA/scenario3.sql
@@ -1,8 +1,10 @@
+/* C:\Users\Christine\Documents\GitHub\Technical\CWA\scenario3.sql */
 /* To sync:
 The mssql extension in Viz Studio can use SQL Server Data Tools to make a new schema comparison between the local database and the LAN one-- or between two server databases. 
 This will identify any differences between the databases.  */
 
-/* I dont actually know if this will work, but you should be able to delete dupes within the CTE using the code below: */
+/* To delete duplicates:
+I don't actually know if this will work, but you should be able to delete dupes within the CTE using the code below: */
 WITH CTE AS(
    SELECT [col1], [col2], [col3], [col4], [col5], [col6], [col7],
        RN = ROW_NUMBER()OVER(PARTITION BY col1 ORDER BY col1)

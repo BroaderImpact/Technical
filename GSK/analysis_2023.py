b48a823c5fb3ac0fa0aa572a4fb41ca5e1c38f1d
BroaderImpact <51309854+BroaderImpact@users.noreply.github.com>
2023-07-13 12:57:25 -0400

    Update analysis_2023.py

--- GSK/analysis_2023.py
+++ GSK/analysis_2023.py
@@ -6,17 +6,17 @@ from transformers import BertTokenizer, BertModel
 
 
 # Syntax of read_table()
-pd.read_table(filepath_or_buffer, sep=NoDefault.no_default, delimiter=None, header='infer', names=NoDefault.no_default, index_col=None, usecols=None, squeeze=None, prefix=NoDefault.no_default, mangle_dupe_cols=True, dtype=None, engine=None, converters=None, true_values=None, false_values=None, skipinitialspace=False, skiprows=None, skipfooter=0, nrows=None, na_values=None, keep_default_na=True, na_filter=True, verbose=False, skip_blank_lines=True, parse_dates=False, infer_datetime_format=False, keep_date_col=False, date_parser=None, dayfirst=False, cache_dates=True, iterator=False, chunksize=None, compression='infer', thousands=None, decimal='.', lineterminator=None, quotechar='"', quoting=0, doublequote=True, escapechar=None, comment=None, encoding=None, encoding_errors='strict', dialect=None, error_bad_lines=None, warn_bad_lines=None, on_bad_lines=None, delim_whitespace=False, low_memory=True, memory_map=False, float_precision=None, storage_options=None)
+pd.read_table('case_study_data.tsv', sep=NoDefault.no_default, delimiter=None, header='infer', names=NoDefault.no_default, index_col=None, usecols=None, squeeze=None, prefix=NoDefault.no_default, mangle_dupe_cols=True, dtype=None, engine=None, converters=None, true_values=None, false_values=None, skipinitialspace=False, skiprows=None, skipfooter=0, nrows=None, na_values=None, keep_default_na=True, na_filter=True, verbose=False, skip_blank_lines=True, parse_dates=False, infer_datetime_format=False, keep_date_col=False, date_parser=None, dayfirst=False, cache_dates=True, iterator=False, chunksize=None, compression='infer', thousands=None, decimal='.', lineterminator=None, quotechar='"', quoting=0, doublequote=True, escapechar=None, comment=None, encoding=None, encoding_errors='strict', dialect=None, error_bad_lines=None, warn_bad_lines=None, on_bad_lines=None, delim_whitespace=False, low_memory=True, memory_map=False, float_precision=None, storage_options=None)
 
 
 # Read TSV file into DataFrame
-df = pd.read_table('courses.tsv')
+df = pd.read_table('case_study_data.tsv')
 print(df)
 
 # columns: uniqueID, drugName, review, date
 
 # EDA: ydata-profiling
-profile = ProfileReport(df, title="Pandas Profiling Report")
+profile = ProfileReport(df, title="Case Study Data Profiling Report")
 
 # Load pre-trained BERT model and tokenizer
 model_name = 'bert-base-uncased'
@@ -24,10 +24,10 @@ tokenizer = BertTokenizer.from_pretrained(model_name)
 model = BertModel.from_pretrained(model_name)
 
 # Sample dataset (assuming you have a DataFrame with 'survey_text' column)
-data = pd.read_csv('customer_feedback.csv')
+data = pd.read_table('case_study_data.tsv')
 
 # Tokenize and encode the survey text using BERT tokenizer
-tokenized_text = data['survey_text'].apply(lambda x: tokenizer.encode(x, add_special_tokens=True))
+tokenized_text = data['review'].apply(lambda x: tokenizer.encode(x, add_special_tokens=True))
 
 # Pad the tokenized sequences to a fixed length
 max_length = max(map(len, tokenized_text))

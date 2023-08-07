264898cffe0295acca9f8b6427ac54cebb54d9f9
BroaderImpact <51309854+BroaderImpact@users.noreply.github.com>
2023-08-07 13:28:58 -0400

    Update pre-proc.py

--- GSK/2023/pre-proc.py
+++ GSK/2023/pre-proc.py
@@ -7,27 +7,7 @@ from nltk.stem import WordNetLemmatizer
 from textblob import TextBlob
 
 # Sample patient review dataset
-data = {
-    'Age': ['30-40', '50-60', '40-50'],
-    'Condition': ['RA', 'COPD', 'SLE'],
-    'Date': ['2022-08-01', '2022-08-02', '2022-08-03'],
-    'Drug': ['DrugA', 'DrugB', 'DrugC'],
-    'DrugId': [101, 102, 103],
-    'EaseofUse': [4.5, 3.8, 4.0],
-    'Effectiveness': [4.2, 3.9, 4.1],
-    'Reviews': [
-        'This drug has been a game-changer for my RA pain! Highly recommend!',
-        'The side effects of DrugB were too severe. Not satisfied with its effectiveness.',
-        'DrugC has significantly improved my SLE symptoms. Very satisfied.'
-    ],
-    'Satisfaction': [5, 2, 5],
-    'Sex': ['Male', 'Female', 'Male'],
-    'SideEffects': [
-        'Nausea, headache', 'Dizziness, dry mouth, insomnia', 'Mild fatigue'
-    ]
-}
-
-df = pd.DataFrame(data)
+df = pd.read_csv('ra.csv')
 
 # Tokenization and Text Normalization
 def preprocess_text(text):

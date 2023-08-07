51d60b737e41742df21edbdb1553d139710f7304
BroaderImpact <51309854+BroaderImpact@users.noreply.github.com>
2023-08-07 13:32:01 -0400

    Update pre-proc.py

--- GSK/2023/pre-proc.py
+++ GSK/2023/pre-proc.py
@@ -1,10 +1,12 @@
-# !pip install -r requirements.txt
+# Install required libraries
+# !pip install pandas nltk textblob
 
 import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from textblob import TextBlob
+import matplotlib.pyplot as plt
 
 # Sample patient review dataset
 df = pd.read_csv('ra.csv')
@@ -41,4 +43,35 @@ def perform_sentiment_analysis(text):
 # Applying Sentiment Analysis to Cleaned_Reviews column
 df['Sentiment'] = df['Cleaned_Reviews'].apply(perform_sentiment_analysis)
 
+# Data Visualization
+# Bar chart for sentiment distribution
+sentiment_counts = df['Sentiment'].value_counts()
+plt.figure(figsize=(8, 6))
+plt.bar(sentiment_counts.index, sentiment_counts.values)
+plt.xlabel('Sentiment')
+plt.ylabel('Count')
+plt.title('Sentiment Analysis Results')
+plt.show()
+
+# Bar chart for top 10 most common conditions
+top_conditions = df['Condition'].value_counts().nlargest(10)
+plt.figure(figsize=(10, 6))
+plt.bar(top_conditions.index, top_conditions.values)
+plt.xlabel('Condition')
+plt.ylabel('Count')
+plt.title('Top 10 Most Common Conditions')
+plt.xticks(rotation=45, ha='right')
+plt.show()
+
+# Word cloud for drug mentions in reviews
+from wordcloud import WordCloud
+drug_mentions = ' '.join(df['Drug'])
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(drug_mentions)
+plt.figure(figsize=(10, 6))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.title('Drug Mentions in Reviews')
+plt.show()
+
+# Display the modified DataFrame with sentiment analysis results
 print(df[['Condition', 'Drug', 'Reviews', 'Sentiment']])
